7073aafbf020c504d50397a711d5408f13fd5510
hyma557 <78861207+hyma557@users.noreply.github.com>
2022-01-11 21:17:40 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,40 @@
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+import statistics
+import random
+import pandas as pd
+import csv
+
+df = pd.read_csv("data.csv")
+data = df["reading_time"].tolist()
+
+def random_set_of_mean(counter):
+    dataset = []
+    for i in range(0, counter):
+        random_index= random.randint(0,len(data)-1)
+        value = data[random_index]
+        dataset.append(value)
+    mean = statistics.mean(dataset)
+    return mean
+
+
+def show_fig(mean_list):
+    df = mean_list
+    fig = ff.create_distplot([df], ["reading_time"], show_hist=False)
+    fig.show()
+
+def setup():
+    mean_list = []
+    for i in range(0,100):
+        set_of_means= random_set_of_mean(30)
+        mean_list.append(set_of_means)
+    show_fig(mean_list)
+    
+    mean = statistics.mean(mean_list)
+    print("Mean of sampling distribution :-",mean )
+
+setup()
+
+population_mean = statistics.mean(data)
+print("population mean:- ", population_mean)
+
